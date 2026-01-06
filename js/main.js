6f3510bd22075615f681241ad20dbb4e97640a54
Ninny <jordansamcooke@gmail.com>
2026-01-06 19:56:15 +1100

    concept of a system to autofil yaml cards

--- js/main.js
+++ js/main.js
@@ -523,3 +523,14 @@ document.addEventListener('keydown', (e) => {
     }
 });
 
+document.querySelectorAll(".collapsible").forEach(button => {
+  button.addEventListener("click", function() {
+ this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+});
